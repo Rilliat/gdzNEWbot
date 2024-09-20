56b061a2f918bb0178011b29ac09beecf8639e93
Rilliat <makarovskiyda@gmail.com>
2024-09-20 23:13:43 +0300

    Update gdz_handlers.py

--- handlers/gdz_handlers.py
+++ handlers/gdz_handlers.py
@@ -16,7 +16,7 @@ def findline(base_str: str, search_str: str) -> list[str]:
     return [x for x in base_str.splitlines() if search_str in x]
 
 
-def findgdz(link, image_src, caption, additional = None) -> MediaGroupBuilder:
+def findgdz(link: str, image_src: str, caption: str, additional :str  = None) -> MediaGroupBuilder:
     builder = MediaGroupBuilder(
         caption=caption
     )
@@ -36,10 +36,12 @@ def findgdz(link, image_src, caption, additional = None) -> MediaGroupBuilder:
     for image in lines:
         if additional:
             image = additional + image
-        builder.add_photo(
-            media='https://' + image
-        )
-        print('https://' + image)
+        image = image.replace('https:', '').replace('//', '')
+        if 'https://' not in image:
+            builder.add_photo(
+                media='https://' + image
+            )
+        print(image)
 
     return builder
 
@@ -80,19 +82,19 @@ async def cmd_geometry(message: Message, command: CommandObject):
 
 @gdz_router.message(Command('physics'))
 async def cmd_physics(message: Message, command: CommandObject):
-    if not command.args:
-        return await message.reply('Нет аргументов! Использование: <code>/physics номер пар/упр/зад/пров/лаб/обс/'
+    args = command.args.split()
+
+    if not command.args or len(args) == 1:
+        return await message.reply('Нет аргументов! Использование: <code>/physics номер пар/упр/зад/лаб/обс/'
                                    'задача *подномер</code>\n'
                                    'Пояснения: \n'
                                    'пар-параграф. /physics номер пар\n'
                                    'упр-упражнение. /physics номер упр подномер\n'
                                    'зад-задание. /physics номер_параграфа зад\n'
-                                   'пров-проверь себя. /physics глава пров\n'
                                    'лаб-лабораторная работа. /physics номер лаб\n'
                                    'обс-обсуди с товарищами. /physics номер_параграфа обс\n'
                                    'задача-задача для повторения. /physics номер задача')
 
-    args = command.args.split()
 
     if not args[0].isnumeric():
         return await message.reply('Первый аргумент - не число!')
@@ -100,15 +102,14 @@ async def cmd_physics(message: Message, command: CommandObject):
     if len(args) > 3:
         return await message.reply('Пожалуйста, запрашивайте только 1 задание в 1 сообщении. ')
 
-    if args[1] not in ['пар', 'упр', 'зад', 'пров', 'лаб', 'обс', 'задача']:
+    if args[1] not in ['пар', 'упр', 'зад', 'лаб', 'обс', 'задача']:
         return await message.reply(
-            'Использование: <code>/physics номер пар/упр/зад/пров/лаб/обс/'
+            'Использование: <code>/physics номер пар/упр/зад/лаб/обс/'
             'задача *подномер</code>\n'
             'Пояснения: \n'
             'пар-параграф. /physics номер пар\n'
             'упр-упражнение. /physics номер упр подномер\n'
             'зад-задание. /physics номер_параграфа зад\n'
-            'пров-проверь себя. /physics глава пров\n'
             'лаб-лабораторная работа. /physics номер лаб\n'
             'обс-обсуди с товарищами. /physics номер_параграфа обс\n'
             'задача-задача для повторения. /physics номер задача')
@@ -117,24 +118,46 @@ async def cmd_physics(message: Message, command: CommandObject):
         caption=f'Вот ваше ГДЗ по Физике',
     )
 
-    match args[1]:
-        case 'пар':
-            r = requests.get(f'https://megaresheba.ru/index/05/0-358/{args[0]}')
+    try:
+        match args[1]:
+            case 'пар':
+                builder = findgdz(f'https://megaresheba.ru/index/05/0-358/{args[0]}',
+                                  'https://megaresheba.ru/attachments/images/tasks/000/003/672/0000/654b5',
+                                  f'Вот ваше гдз для {args[0]} параграфа по Физике')
+            case 'упр':
+                builder = findgdz(f'https://megaresheba.ru/index/05/0-358/2-{args[0]}-{args[2]}',
+                                  'https://megaresheba.ru/attachments/images/tasks/000/003/672/0000/654b5',
+                                  f'Вот ваше гдз для {args[0]} упражнения подномер {args[2]} по Физике')
+            case 'зад':
+                builder = findgdz(f'https://megaresheba.ru/index/05/0-358/3-{args[0]}',
+                                  'https://megaresheba.ru/attachments/images/tasks/000/003/672/0000/654b5',
+                                  f'Вот ваше гдз для задания из параграфа {args[0]} по Физике')
+            case 'лаб':
+                builder = findgdz(f'https://megaresheba.ru/index/05/0-358/5-{args[0]}',
+                                  'https://megaresheba.ru/attachments/images/tasks/000/003/672/0000/654b5',
+                                  f'Вот ваше гдз для лабораторной работы {args[0]} по Физике')
+            case 'обс':
+                builder = findgdz(f'https://megaresheba.ru/index/05/0-358/6-{args[0]}',
+                                  'https://megaresheba.ru/attachments/images/tasks/000/003/672/0000/654b5',
+                                  f'Вот ваше гдз для "обсуди с товарищами" из параграфа {args[0]} по Физике')
+            case 'задача':
+                builder = findgdz(f'https://megaresheba.ru/index/05/0-358/7-{args[0]}',
+                                  'https://megaresheba.ru/attachments/images/tasks/000/003/672/0000/654b5',
+                                  f'Вот ваше гдз для задачи {args[0]} по Физике')
+
+    except IndexError:
+        await message.reply(
+            'Использование: <code>/physics номер пар/упр/зад/лаб/обс/'
+            'задача *подномер</code>\n'
+            'Пояснения: \n'
+            'пар-параграф. /physics номер пар\n'
+            'упр-упражнение. /physics номер упр подномер\n'
+            'зад-задание. /physics номер_параграфа зад\n'
+            'лаб-лабораторная работа. /physics номер лаб\n'
+            'обс-обсуди с товарищами. /physics номер_параграфа обс\n'
+            'задача-задача для повторения. /physics номер задача')
 
 
-
-
-
-    r = requests.get(f'https://gdz.top/7-klass/geometrija/atanasjan-fgos/{args[0]}')
-    lines = findline(r.text, '<img src="/geometrija_07/atanasjan-fgos/1-00/')
-
-    for i_line in range(len(lines)):
-        lines[i_line] = lines[i_line].strip().split('"')[1]
-
-    for gdz in lines:
-        builder.add_photo(
-            media='https://gdz.top/' + gdz,
-        )
     await message.reply_media_group(builder.build())
 
 
